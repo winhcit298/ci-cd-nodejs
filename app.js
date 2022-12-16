1cf25c5947f00db1a411a9d8ae638cc03dc2c940
unknown <winhcit298@gmail.com>
2022-12-16 22:25:06 +0700

    Edit code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const createError = require('http-errors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', (req, res) => res.send('Hello World vinh!'));
+app.use('/', (req, res) => res.send('Hello World thay Y!'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
